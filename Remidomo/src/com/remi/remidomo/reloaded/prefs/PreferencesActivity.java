b0c301b15ae1bca9172f1ba64a2d08f5529bae32
Remi Peuvergne <rpeuvergne@gmail.com>
2013-05-10 22:31:54 +0200

    Prefs: enable the "back" button in ActionBar

--- Remidomo/src/com/remi/remidomo/reloaded/prefs/PreferencesActivity.java
+++ Remidomo/src/com/remi/remidomo/reloaded/prefs/PreferencesActivity.java
@@ -5,10 +5,19 @@ import java.util.List;
 import com.remi.remidomo.reloaded.R;
 
 import android.app.backup.BackupManager;
+import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.view.MenuItem;
 
 public class PreferencesActivity extends PreferenceActivity {
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Enable the "back" arrow in action bar
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+	}
 	@Override
 	public void onBuildHeaders(List<Header> target) {
 		loadHeadersFromResource(R.xml.pref_headers, target);
@@ -20,5 +29,16 @@ public class PreferencesActivity extends PreferenceActivity {
 		new BackupManager(this).dataChanged();
 
 		super.onPause();
+	}
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			finish();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 }
